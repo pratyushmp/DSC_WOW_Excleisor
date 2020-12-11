9e6e4f371230a7757c2832f81b0f5753d6ca9ae5
infiniteoverflow <aswingopinathan1871@gmail.com>
2020-12-11 14:05:59 +0530

    Added Google and FB Auth codes

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -66,4 +66,48 @@ Future registerUser(String email,String pass) async{
   }
 }
 
+Future<UserCredential> signInWithGoogle() async {
+  await Firebase.initializeApp();
+  // Trigger the authentication flow
+  final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
 
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+  // Create a new credential
+  final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
+
+
+
+Future<UserCredential> signInWithFacebook() async {
+  // try {
+    await Firebase.initializeApp();
+    // Trigger the sign-in flow
+    var result;
+    try {
+      result = await FacebookAuth.instance.login();
+    }
+    catch(e) {
+      print('exception: $e');
+    }
+    if(result == null) return null;
+    // Create a credential from the access token
+    final FacebookAuthCredential facebookAuthCredential =
+    FacebookAuthProvider.credential(result.token);
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(
+        facebookAuthCredential);
+  // }
+  // on FacebookAuthException catch(e) {
+  //   return null;
+  //
+  // }
+}

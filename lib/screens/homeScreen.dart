4a3904c50b45f7811a5497c29eb18c39d2dce6da
infiniteoverflow <aswingopinathan1871@gmail.com>
2020-12-12 15:22:55 +0530

    Added SecondScreen

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -2,6 +2,7 @@ import 'package:befikr_app/screens/infoScreen.dart';
 import 'package:befikr_app/screens/authScreen.dart';
 import 'package:befikr_app/screens/onboardingScreen.dart';
 import 'package:befikr_app/screens/permissionScreen.dart';
+import 'package:befikr_app/screens/secondScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:ink_page_indicator/ink_page_indicator.dart';
 
@@ -22,7 +23,7 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    controller = PageIndicatorController()
+    controller = PageIndicatorController(initialPage: 1)
       ..addListener(() {
         page.value = controller.page;
       });
@@ -30,8 +31,8 @@ class _HomeState extends State<Home> {
 
   List<Widget> _createChildren(int count) {
     final List<Widget> result = [
-      AuthScreen(),
       InfoPage(),
+      SecondScreen(),
       Permission()
 
 
@@ -40,27 +41,18 @@ class _HomeState extends State<Home> {
   }
 
   Widget buildInkPageIndicator(InkStyle style) {
-    return Expanded(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            child: InkPageIndicator(
-              gap: 20,
-              padding: 16,
-              shape: shape,
-              page: page,
-              pageCount: 3,
-              activeShape: activeShape,
-              inactiveColor: Colors.grey.shade500,
-              activeColor: Colors.grey.shade700,
-              inkColor: Colors.grey.shade400,
-              style: style,
-            ),
-          ),
-        ],
-      ),
+    return InkPageIndicator(
+      gap: 20,
+      padding: 16,
+      shape: shape,
+      page: page,
+      pageCount: 3,
+      activeShape: activeShape,
+      controller: controller,
+      inactiveColor: Colors.grey.shade500,
+      activeColor: Colors.grey.shade700,
+      inkColor: Colors.grey.shade400,
+      style: style,
     );
   }
 
@@ -72,26 +64,24 @@ class _HomeState extends State<Home> {
     activeShape = IndicatorShape.circle(5);
 
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              buildInkPageIndicator(InkStyle.normal),
-            ],
-          ),
-          InkWell(
-            onTap: () => controller.animateToPage(
-              controller.page != children.length - 1 ? children.length - 1 : 0,
-              duration: const Duration(milliseconds: 800),
-              curve: Curves.ease,
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            InkWell(
+              child: PageView(
+                controller: controller,
+                children: children,
+              ),
             ),
-            child: PageView(
-              controller: controller,
-              children: children,
+            Column(
+              children: <Widget>[
+                buildInkPageIndicator(InkStyle.simple),
+              ],
             ),
-          ),
+            
 
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/screens/secondScreen.dart
@@ -0,0 +1,79 @@
+import 'package:befikr_app/widgets/circlePainter.dart';
+import 'package:befikr_app/widgets/curve_wave.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
+
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key key, this.size = 80.0, this.color = Colors.red,
+    this.onPressed,this.child,}) : super(key: key);
+  final double size;
+  final Color color;
+  final Widget child;
+  final VoidCallback onPressed;
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> with TickerProviderStateMixin {
+  AnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    )..repeat();
+  }
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  Widget _button() {
+    return Center(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(widget.size),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: <Color>[
+                widget.color,
+                Color.lerp(widget.color, Colors.black, .05)
+              ],
+            ),
+          ),
+          child: ScaleTransition(
+              scale: Tween(begin: 0.95, end: 1.0).animate(
+                CurvedAnimation(
+                  parent: _controller,
+                  curve: const CurveWave(),
+                ),
+              ),
+              child: Icon(Icons.speaker_phone, size: 44,)
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CustomPaint(
+          painter: CirclePainter(
+            _controller,
+            color: widget.color,
+          ),
+          child: SizedBox(
+            width: widget.size * 4.125,
+            height: widget.size * 4.125,
+            child: _button(),
+          ),
+        ),
+      ),
+    );
+  }
+}
